492f6d7bdb1c30717d9f966f0a8f307c17ebd9a0
benzinoh <benjamin.ejike@yahoo.com>
2023-11-01 21:28:20 -0400

    new

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = "ca-central-1"
+  access_key = "AKIATY4MFKWAL42N2GXH"
+  secret_key = "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"
+
+}
---
+++ testec2.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "new_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+}
+# Create AWS instance
+resource "aws_instance" "testec2" {
+  ami           = data.aws_ami.new_ami.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tf-ec2"
+  }
+}
