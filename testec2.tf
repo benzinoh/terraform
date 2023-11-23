db02b1357f711199a374f20c286aa078cb34d763
Benjamin <78206468+benzinoh@users.noreply.github.com>
2023-11-22 23:07:24 -0500

    Update testec2.tf

--- testec2.tf
+++ testec2.tf
@@ -1,18 +1,6 @@
-data "aws_ami" "new_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-}
-# Create AWS instance
 resource "aws_instance" "testec2" {
-  ami           = data.aws_ami.new_ami.id
+  ami           = "ami-0f17d6a8a3d746af6"
   instance_type = "t2.micro"
   tags = {
     Name = "tf-ec2"
   }
-}
