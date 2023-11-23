5b7ac2616892fd4fcf10763de54a1367e9c381c8
Benjamin <78206468+benzinoh@users.noreply.github.com>
2023-11-22 23:13:19 -0500

    Update testec2.tf

--- testec2.tf
+++ testec2.tf
@@ -1,6 +1,11 @@
+provider "aws" {
+  region     = "ca-central-1"
+}
+
 resource "aws_instance" "testec2" {
   ami           = "ami-0f17d6a8a3d746af6"
   instance_type = "t2.micro"
+  key_name      = "terraform"
   tags = {
     Name = "tf-ec2"
   }
